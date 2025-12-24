a818f3de8feb1eccbe6151f0cb2e94472c314863
jLun4rd1 <joaoclunardi@gmail.com>
2025-12-24 07:51:58 -0300

    :sparkles: feat - add collision check between asteroids and shots

--- main.py
+++ main.py
@@ -50,6 +50,13 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
