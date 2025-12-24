5fef19d69f7f1b2adc92dd0031fcd781f4458024
jLun4rd1 <joaoclunardi@gmail.com>
2025-12-24 13:39:09 -0300

    :sparkles: feat - add split function to Asteroids

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def main():
                 if shot.collides_with(asteroid):
                     log_event("asteroid_shot")
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         pygame.display.flip()
 
